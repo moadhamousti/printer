f46f63115d956ba14bd4a4649d0e647631d82feb
moadhamousti <moadhamousti@gmail.com>
2024-06-02 18:50:47 +0100

    Update Landing Page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,10 @@ import { ArrowRight } from 'lucide-react'
 // import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 const Navbar = async () => {
-//   const { getUser } = getKindeServerSession()
-//   const user = await getUser()
-    const user = undefined
+  // const { getUser } = getKindeServerSession()
+  // const user = await getUser()
 
-//   const isAdmin = user?.email === process.env.ADMIN_EMAIL
+  // const isAdmin = user?.email === process.env.ADMIN_EMAIL
 
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
@@ -20,17 +19,37 @@ const Navbar = async () => {
           </Link>
 
           <div className='h-full flex items-center space-x-4'>
-            {user ? (
-              <>
-                <Link href='/api/auth/logout' 
-                className={buttonVariants({
+            {/* {user ? (
+              <> */}
+                <Link
+                  href='/api/auth/logout'
+                  className={buttonVariants({
                     size: 'sm',
                     variant: 'ghost',
                   })}>
-                  Logout
+                  Sign out
                 </Link>
-              </>
-            ) : (
+                {/* {isAdmin ? ( */}
+                  <Link
+                    href='/dashboard'
+                    className={buttonVariants({
+                      size: 'sm',
+                      variant: 'ghost',
+                    })}>
+                    Dashboard ✨
+                  </Link>
+                {/* ) : null} */}
+                <Link
+                  href='/configure/upload'
+                  className={buttonVariants({
+                    size: 'sm',
+                    className: 'hidden sm:flex items-center gap-1',
+                  })}>
+                  Create case
+                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                </Link>
+              {/* </> */}
+            {/* ) : ( */}
               <>
                 <Link
                   href='/api/auth/register'
@@ -62,7 +81,7 @@ const Navbar = async () => {
                   <ArrowRight className='ml-1.5 h-5 w-5' />
                 </Link>
               </>
-            )}
+            {/* )} */}
           </div>
         </div>
       </MaxWidthWrapper>
