7cc81b94f524c9dfb77dc6d6ba969b859659c31e
moadhamousti <moadhamousti@gmail.com>
2024-06-09 19:54:35 +0100

    Update

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,7 @@
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
 import { z } from 'zod'
 import sharp from 'sharp'
-// import { db } from '@/db'
+import { db } from '@/db'
 
 const f = createUploadthing()
 
@@ -11,37 +11,37 @@ export const ourFileRouter = {
     .middleware(async ({ input }) => {
       return { input }
     })
-    .onUploadComplete(async ({ file }) => {
-      // const { configId } = metadata.input
+    .onUploadComplete(async ({ metadata, file }) => {
+      const { configId } = metadata.input
 
       const res = await fetch(file.url)
-      // const buffer = await res.arrayBuffer()
+      const buffer = await res.arrayBuffer()
 
-      // const imgMetadata = await sharp(buffer).metadata()
-      // const { width, height } = imgMetadata
+      const imgMetadata = await sharp(buffer).metadata()
+      const { width, height } = imgMetadata
 
-      // if (!configId) {
-      //   const configuration = await db.configuration.create({
-      //     data: {
-      //       imageUrl: file.url,
-      //       height: height || 500,
-      //       width: width || 500,
-      //     },
-      //   })
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        })
 
-      //   return { configId: configuration.id }
-      // } else {
-      //   const updatedConfiguration = await db.configuration.update({
-      //     where: {
-      //       id: configId,
-      //     },
-      //     data: {
-      //       croppedImageUrl: file.url,
-      //     },
-      //   })
+        return { configId: configuration.id }
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        })
 
-      //   return { configId: updatedConfiguration.id }
-      // }
+        return { configId: updatedConfiguration.id }
+      }
     }),
 } satisfies FileRouter
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import { Toaster } from '@/components/ui/toaster'
+import Providers from '@/components/Providers'
 
 const recursive = Recursive({ subsets: ['latin'] })
 
@@ -19,7 +20,7 @@ export default function RootLayout({
 
         <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
           <div className='flex-1 flex flex-col h-full'>
-            {children}
+            <Providers>{children}</Providers>
           </div>
           <Footer />
         </main>
